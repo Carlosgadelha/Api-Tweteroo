043ea4d67b3f1a028733e67e4ab51bd3c00960e6
Carlos Gadelha <josecarlos.gadelha10@gmail.com>
2022-04-22 21:44:22 -0300

    adicionando get e post  --> /tweets

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
+
 app.use(express.json());
 app.use(cors())
+
 let usuarios = []
+let tweets = []
 
 
 app.post('/sign-up', (req, res) => {
     const usuario = req.body
     usuarios = [... usuarios, usuario]
-    console.log(usuarios);
     res.send("OK")
 });
 
+app.post('/tweets', (req, res) => {
+    const tweet = req.body
+    tweets = [... tweets, tweet]
+    res.send("OK")
+});
 
-
+app.get('/tweets', (req, res) => {
+    let tweetsFiltrados = []
+    tweets.forEach(tweet => {
+        usuarios.find(usuario => {
+            if(usuario.username === tweet.username){
+                tweetsFiltrados = [... tweetsFiltrados,{
+                    username: tweet.username,
+                    avatar: usuario.avatar,
+                    tweet: tweet.tweet
+                }]
+            }})
+    })
+    res.send(tweetsFiltrados)
+});
 
 app.listen(5000,()=>{
     console.log('Server is running on port 5000');
