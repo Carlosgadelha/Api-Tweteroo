ce66a251409f831353439ca93805971df17e58cc
Carlos Gadelha <josecarlos.gadelha10@gmail.com>
2022-04-24 11:35:25 -0300

    validação dos usuario e tweet

--- index.js
+++ index.js
@@ -11,31 +11,51 @@ let tweets = []
 
 app.post('/sign-up', (req, res) => {
     const usuario = req.body
-    usuarios = [... usuarios, usuario]
-    res.send("OK")
+
+    if(usuario.username === '' || usuario.avatar === ''){
+        res.sendStatus(400)
+    }else{
+        usuarios = [... usuarios, usuario]
+        res.send("OK")
+    }
+    
 });
 
 app.post('/tweets', (req, res) => {
+    
+    if(req.body.tweet === ''){
+        res.sendStatus(400, "Todos os campos são obrigatórios!")
+    }
+    req.header('User', req.body.user)
     const tweet = req.body
     tweets = [... tweets, tweet]
+    res.sendStatus(201)
     res.send("OK")
 });
 
-app.get('/tweets', (req, res) => {
-    let tweetsFiltrados = []
-    
-    if (tweets.length > 10) tweets = tweets.slice(tweets.length - 10, tweets.length)
+app.get('/tweets:page', (req, res) => {
+    const {page} = req.params
+    if(parseInt(page) > 1){
 
-    tweets.forEach(tweet => {
-        usuarios.find(usuario => {
-            if(usuario.username === tweet.username){
-                tweetsFiltrados = [... tweetsFiltrados,{
-                    username: tweet.username,
-                    avatar: usuario.avatar,
-                    tweet: tweet.tweet
-                }]
-            }})
-    })
+        if (tweets.length > 10) tweets = tweets.slice(tweets.length - 10, tweets.length)
+
+        tweets.forEach(tweet => {
+            usuarios.find(usuario => {
+                if(usuario.username === tweet.username){
+                    tweetsFiltrados = [... tweetsFiltrados,{
+                        username: tweet.username,
+                        avatar: usuario.avatar,
+                        tweet: tweet.tweet
+                    }]
+                }})
+        })
+
+    } else{
+        res.sendStatus(400, "Informe uma página válida!")
+    }
+    let tweetsFiltrados = []
+
+    
     res.send(tweetsFiltrados)
 });
 
